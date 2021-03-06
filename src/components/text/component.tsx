ecece60a9a11c02bf943c153897088fd4daf6b44
red-runner <pol@lodgify.com>
2021-03-06 19:37:33 +0100

    feat(Text): expose text

---
+++ src/components/text/component.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from "react";
+import classnames from "classnames";
+
+import { StylesWrapper } from "./styles";
+
+enum Sizes {
+  Regular = "regular",
+  Small = "small",
+}
+
+enum Alignment {
+  Left = "left-aligned",
+  Center = "centered",
+  Right = "right-aligned",
+}
+
+type Props = {
+  children: string;
+  size?: Sizes;
+  alignment?: Alignment;
+};
+
+type TextComponent<P> = FC<P> & {
+  Sizes: typeof Sizes;
+  Alignment: typeof Alignment;
+};
+
+export const Component: TextComponent<Props> = ({
+  children,
+  size = Sizes.Regular,
+  alignment = Alignment.Left,
+}: Props) => (
+  <StylesWrapper
+    className={classnames("text", size as string, alignment as string)}
+  >
+    {children}
+  </StylesWrapper>
+);
+
+Component.displayName = "Text";
+Component.Sizes = Sizes;
+Component.Alignment = Alignment;
