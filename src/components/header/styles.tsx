c24897e51e37d14913f7f4e88a4cfc26288cdf26
red-runner <pol@lodgify.com>
2021-03-07 19:49:22 +0100

    feat(Header): expose component

---
+++ src/components/header/styles.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const StylesWrapper = styled.header`
+  display: flex;
+  background: var(--white);
+  align-items: center;
+  padding: 5px;
+  box-shadow: 0 1px 0 0 var(--grey);
+  gap: 0.625rem;
+  height: 3.75rem;
+  position: sticky;
+  top: 0;
+
+  figure {
+    height: inherit;
+    width: 3.75rem;
+    margin: 0;
+
+    img {
+      object-fit: cover;
+      height: 100%;
+      width: 100%;
+    }
+  }
+
+  h4 {
+    margin-bottom: 8px;
+  }
+`;
